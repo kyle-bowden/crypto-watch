c43d036536cb746e0f079b092ded9b888b471c00
Kyle Bowden <1393788+kyle-bowden@users.noreply.github.com>
2022-01-30 21:21:55 +0000

    Add proxy only for static images

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -14,29 +14,29 @@ module.exports = app => {
         })
     );
 
-    app.use(
-        '/api/v3/**',
-        createProxyMiddleware({
-            target: 'https://api.coingecko.com',
-            changeOrigin: true,
-            onProxyReq: function onProxyReq(proxyReq, req, res) {
-                // Log outbound request to remote target
-                console.log('-->  ', req.method, req.path, '->', proxyReq.baseUrl + proxyReq.path);
-            }
-        })
-    );
-
-    app.use(
-        '/api/**',
-        createProxyMiddleware({
-            target: 'https://finnhub.io',
-            changeOrigin: true,
-            onProxyReq: function onProxyReq(proxyReq, req, res) {
-                // Log outbound request to remote target
-                console.log('-->  ', req.method, req.path, '->', proxyReq.baseUrl + proxyReq.path);
-            }
-        })
-    );
+    // app.use(
+    //     '/api/v3/**',
+    //     createProxyMiddleware({
+    //         target: 'https://api.coingecko.com',
+    //         changeOrigin: true,
+    //         onProxyReq: function onProxyReq(proxyReq, req, res) {
+    //             // Log outbound request to remote target
+    //             console.log('-->  ', req.method, req.path, '->', proxyReq.baseUrl + proxyReq.path);
+    //         }
+    //     })
+    // );
+    //
+    // app.use(
+    //     '/api/**',
+    //     createProxyMiddleware({
+    //         target: 'https://finnhub.io',
+    //         changeOrigin: true,
+    //         onProxyReq: function onProxyReq(proxyReq, req, res) {
+    //             // Log outbound request to remote target
+    //             console.log('-->  ', req.method, req.path, '->', proxyReq.baseUrl + proxyReq.path);
+    //         }
+    //     })
+    // );
 
     app.use(morgan('combined'));
 };
